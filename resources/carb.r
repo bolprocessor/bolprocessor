9be4830a1ffaaf635246daa3a42d6bbccfce6870
Anthony Kozar <akozar@users.sourceforge.net>
2007-01-26 21:08:45 +0000

    New files for 'carb' and 'plst' resources.

---
+++ resources/carb.r
@@ -0,0 +1,9 @@
+/* carb.r
+
+   The 'carb' resource allows a Carbon application to launch as
+   a native OS X app instead of within the Classic environment
+ */
+
+type 'carb' {};
+
+resource 'carb'(0) {};
