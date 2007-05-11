8e00526498b9e1d4bc5ddf3f38eff42495d67aa8
Anthony Kozar <akozar@users.sourceforge.net>
2007-05-11 20:53:19 +0000

    #defines for turning off unneeded WASTE features.

--- source/prefix/PrefixTransWASTE.h
+++ source/prefix/PrefixTransWASTE.h
@@ -12,6 +12,13 @@
 
 // 1 to use WASTE for text editing, 0 to use TextEdit instead
 #define WASTE 1
+// this is used by WASTE to cut out some non-Carbon-compliant code
+#define SystemSevenFiveOrLater 1
+// turn off WASTE features that we do not need
+#define WASTE_OBJECTS  0
+#define WASTE_USE_UPPS 0
 
 // disable built-in MIDI driver at compile time (not finished yet) - 010507 akozar
 #define USE_BUILT_IN_MIDI_DRIVER 1
+
+#define EXPERIMENTAL 1
