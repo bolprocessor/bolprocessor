36a93b3d6dbb238b45011943bbabe15964af1725
Anthony Kozar <akozar@users.sourceforge.net>
2020-08-26 17:05:47 -0400

    New prefix file for building BP3 console app with CoreMIDI driver.

---
+++ source/prefix/PrefixANSICoreMidiDebug.h
@@ -0,0 +1,45 @@
+/*	PrefixANSICoreMidiDebug.h
+
+	BP3 Console App with CoreMIDI Prefix Header
+ */
+
+// CoreServices includes the Carbon MacTypes.h header.
+// Including it here and #ifdef-ing dupicate definitions in
+// MissingMacTypes.h avoids conflicts in CoreMIDIdriver.c.
+#include <CoreServices/CoreServices.h>
+
+// our replacements for various Mac types
+#include "MissingMacTypes.h"
+#include "MissingMacPrototypes.h"
+#include "CTextHandles.h"
+
+// don't compile any GUI-specific code
+#define BP_CARBON_GUI 0
+
+// our macro to indicate a Mach-O compile
+#define BP_MACHO 1
+
+// 1 for debugging memory, 0 otherwise
+#define BIGTEST 1
+
+// 1 to execute Beta tests for data validity, 0 otherwise
+#define COMPILING_BETA 1
+
+// 1 to use WASTE for text editing, 0 to use TextEdit instead
+#define WASTE 0
+// this is used by WASTE to cut out some non-Carbon-compliant code
+#define SystemSevenFiveOrLater 1
+// turn off WASTE features that we do not need
+#define WASTE_OBJECTS  0
+#define WASTE_USE_UPPS 0
+
+// disable built-in MIDI driver and OMS Midi driver
+#define USE_BUILT_IN_MIDI_DRIVER 0
+#define USE_OMS 0
+
+// enable null MIDI driver
+#define WITH_REAL_TIME_MIDI 1
+
+#define EXPERIMENTAL 0
+
+#define TRACE_EVENTS 0
